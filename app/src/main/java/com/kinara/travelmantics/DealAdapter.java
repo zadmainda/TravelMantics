0509e663391da923054f259278f78e86f8ddc6cc
zadock <kinara.zadock@gmail.com>
2019-08-06 16:26:36 +0300

    add the New Layout

--- app/src/main/java/com/kinara/travelmantics/DealAdapter.java
+++ app/src/main/java/com/kinara/travelmantics/DealAdapter.java
@@ -85,96 +85,20 @@ public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHolder
 
     public class DealViewHolder extends RecyclerView.ViewHolder {
         TextView tvTitle;
+        TextView tvDescription;
+        TextView tvPrice;
 
        public DealViewHolder(@NonNull View itemView) {
            super(itemView);
            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+           tvDescription = (TextView) itemView.findViewById(R.id.tvDescription);
+           tvPrice = (TextView) itemView.findViewById(R.id.tvPrice);
        }
 
        public void bind(TravelDeal deal){
             tvTitle.setText(deal.getTitle());
+            tvDescription.setText(deal.getDescription());
+            tvPrice.setText(deal.getPrice());
        }
    }
 }
-
-
-
-
-
-/*
-public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHolder> {
-    ArrayList<TravelDeal> deals;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mDatabaseReference;
-    private ChildEventListener mChildListener;
-
-    public  DealAdapter (){
-        FirebaseUtil.openFbReference("traveldeals");
-        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
-        mDatabaseReference = FirebaseUtil.mDatabaseReference;
-        deals = FirebaseUtil.mDeals;
-        mChildListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
-                Log.d("Deal: ", td.getTitle());
-                td.setId(dataSnapshot.getKey());
-                deals.add(td);
-                notifyItemInserted(deals.size()-1);
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-        mDatabaseReference.addChildEventListener(mChildListener);
-
-    }
-
-    @NonNull
-    @Override
-    public DealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
-        View itemView = LayoutInflater.from(context).inflate(R.layout.rv_row, parent, false);
-        return  new DealViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull DealViewHolder holder, int position) {
-        TravelDeal deal = deals.get(position);
-        holder.bind(deal);
-    }
-
-    @Override
-    public int getItemCount() {
-        return deals.size();
-    }
-
-    public class DealViewHolder extends RecyclerView.ViewHolder {
-        TextView tvTitle;
-        public DealViewHolder(@NonNull View itemView) {
-            super(itemView);
-            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
-        }
-        public void bind(TravelDeal deal){
-            tvTitle.setText(deal.getTitle());
-        }
-    }
-}
-*/
