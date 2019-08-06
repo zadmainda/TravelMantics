8304e8788268fb12b3bfb6b268f610d7740ed3b8
zadock <kinara.zadock@gmail.com>
2019-08-06 12:45:04 +0300

    Showing n reading data into a text view

--- app/src/main/java/com/kinara/travelmantics/ListActivity.java
+++ app/src/main/java/com/kinara/travelmantics/ListActivity.java
@@ -25,10 +25,44 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        RecyclerView rvDeals = (RecyclerView) findViewById(R.id.rvDeals);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        mChildListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                TextView tvDeals = (TextView) findViewById(R.id.tvDeals);
+                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
+                tvDeals.setText(tvDeals.getText() + "\n" + td.getTitle());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildListener);
+
+
+
+        /*RecyclerView rvDeals = (RecyclerView) findViewById(R.id.tvDeals_recycler);
         final DealAdapter adapter = new DealAdapter();
         rvDeals.setAdapter(adapter);
         LinearLayoutManager dealsLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        rvDeals.setLayoutManager(dealsLayoutManager);
+        rvDeals.setLayoutManager(dealsLayoutManager);*/
     }
 }
